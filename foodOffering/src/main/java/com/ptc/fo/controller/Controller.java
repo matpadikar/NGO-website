9c094d8176357882b7c16e2ea8f1d0a9aec1286c
pratikshaEm7 <41942536+pratikshaEm7@users.noreply.github.com>
2018-08-06 12:34:12 +0530

    FO

--- foodOffering/src/main/java/com/ptc/fo/controller/Controller.java
+++ foodOffering/src/main/java/com/ptc/fo/controller/Controller.java
@@ -1,8 +1,14 @@
 package com.ptc.fo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.validator.internal.util.privilegedactions.GetResource;
+import org.hibernate.validator.internal.util.privilegedactions.GetResources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +19,23 @@ import com.ptc.fo.model.Restaurant;
 import com.ptc.fo.repo.ResRepo;
 
 @org.springframework.stereotype.Controller
-@RequestMapping("/foodOffering")
 public class Controller {
 
 	@Autowired
 	ResRepo resRepo;
-
+	/*List<Restaurant> res= new ArrayList<Restaurant>();*/
 	@ResponseBody
+	/*@GetMapping(path="/find")
+	public ResponseEntity GetResource() {
+		ResponseEntity res = new ResponseEntity<>()
+		return res;
+		
+	}*/
+	@GetMapping("/")
+	public String getIndex() {
+		return "index";
+	}
+	
 	@PostMapping(path = "/add", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity getAll(@RequestBody User model) {
 		Restaurant res = new Restaurant();
